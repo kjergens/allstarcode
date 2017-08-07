3c0f242b73d4beaf66854f6d9401d3a8888ae189
Katie Jergens <katie.jergens@gmail.com>
2017-08-07 13:29:55 -0400

    rps - highscore

--- highscore/js/script.js
+++ highscore/js/script.js
@@ -1,9 +1,10 @@
 var initials = '';
 var playerWeapon = '';
 var computerWeapon = '';
-var weapons = ['rock','paper','scissors'];
+var weapons = ['Kevin','Mahdi','Roger'];
 var score = 0;
 var database = firebase.database().ref();
+var verbs = ['annihilates', 'destroys', 'embarrasses', 'teaches a lesson to', 'beats', 'wallops', 'overcomes']
 
 function getInitials() {
 	initials = $("#initialsInput").val();
@@ -11,54 +12,68 @@ function getInitials() {
 }
 
 function rockSelected() {
-	playerWeapon = 'rock';
+	playerWeapon = 'Kevin';
 	finishTurn();
 }
 
 function paperSelected() {
-	playerWeapon = 'paper';
+	playerWeapon = 'Mahdi';
 	finishTurn();
 }
 
 function scissorsSelected() {
-	playerWeapon = 'scissors';
+	playerWeapon = 'Roger';
 	finishTurn();
 }
 
-function setComputerWeapon() {
-	computerWeapon = weapons[Math.floor(Math.random()*weapons.length)];
-	$("#computer-weapon").text("Computer chooses "+computerWeapon)
-}
-
 function finishTurn() {
-	$("#player-weapon").text("You choose "+playerWeapon)
+	$("#player-weapon").text("You chose "+playerWeapon+". ")
 	setComputerWeapon();
 	determineWinner();
 }
+
+
+function setComputerWeapon() {
+	computerWeapon = weapons[Math.floor(Math.random()*weapons.length)];
+	$("#computer-weapon").text("Computer chooses "+computerWeapon+". ")
+}
+
+
 
 
 function determineWinner() {
 
 	let oldScore = score;
 
-	if(playerWeapon==='rock') {
-		if(computerWeapon==='scissors') {
+	if(playerWeapon==='Kevin') {
+		if(computerWeapon==='Roger') {
 			score++;
 		} 
-	} else if (playerWeapon==='paper') {
-		if (computerWeapon==='rock') {
+	} else if (playerWeapon==='Mahdi') {
+		if (computerWeapon==='Kevin') {
 			score++;
 		}
-	} else if (playerWeapon==='scissors') {
-		if (computerWeapon==='paper') {
+	} else if (playerWeapon==='Roger') {
+		if (computerWeapon==='Mahdi') {
 			score++;
 		}
 	}
 
 	if (score>oldScore) {
 		$("#who-won").text("You win!")
+		$("#description").text(playerWeapon+" "+verbs[Math.floor(Math.random()*verbs.length)]+" "+computerWeapon+"!")
+		$("#result_container").css("background-color", "#d3f1d8")
+		$("#result_container").css("color", "#435b46")
+	} else if (playerWeapon !== computerWeapon){
+		$("#description").text(computerWeapon+" "+verbs[Math.floor(Math.random()*verbs.length)]+" "+playerWeapon+"!")
+		$("#who-won").text("Computer wins.")
+		$("#result_container").css("background-color", "#e0b2b2")
+		$("#result_container").css("color", "#990000")
 	} else {
-		$("#who-won").text("")
+		$("#description").text('')
+		$("#result_container").css("background-color", "#eee")
+		$("#result_container").css("color", "#333")
+		$("#who-won").text("Tie.")
 	}
 
 	$("#score").text("Your score: "+score);
@@ -85,7 +100,7 @@ database.orderByChild("SCORE").limitToLast(10).on("child_added", function(rowDat
   		//$("#leaderboard").append("<div>"+line+"</div>");
 
   		// Write top score at top
-  		let board=`<div>${line}</div>`+$("#leaderboard").html();
+  		let board=`<li class="list-group-item">${line}</li>`+$("#leaderboard").html();
   		$("#leaderboard").html(board);
 });
 
